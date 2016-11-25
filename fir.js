26de610cb52503de5856d8e9f026e1adbefc314c
Vinay S <vinay.s.9993@gmail.com>
2016-11-25 23:46:36 +0530

    Add files via upload

---
+++ fir.js
@@ -0,0 +1,62 @@
+function quesone(){
+  var a=document.getElementById("quesone");
+ 
+a.innerHTML="I can wink at you blink my eyes and kiss and smile at the same time!!";
+  a.style.color="red";
+}
+ function questwo(){  
+  var b=document.getElementById("questwo");
+b.innerHTML="Yes!! See My face. It turns pink whenever i see you";
+  b.style.color="red";
+ }
+
+function questhree(){
+  var c=document.getElementById("questhree");
+c.innerHTML="I squeez my belly to make it look more like 6pack. Look at me I will show you!!";
+  c.style.color="red";
+}
+
+function quesfour(){
+  var d=document.getElementById("quesfour");
+d.innerHTML="Infinite!!";
+  d.style.color="red";
+}
+
+function quesfive(){
+  var e=document.getElementById("quesfive");
+e.innerHTML="I am born Naughty baby!!";
+  e.style.color="red";
+}
+
+function eyes(){
+  var f = document.getElementById("gogglesleft");
+f.style.animationName="goggles";
+  f.style.animationDuration="1s";
+  f.style.animationIterationCount="3";
+  f.style.animationDelay="6s";
+}
+
+
+function colors(){
+  var g = document.getElementById("face");
+g.style.animationName="colors";
+  g.style.animationDuration="3s";
+  g.style.animationIterationCount="3";
+   g.style.animationDelay="6s";
+ }
+
+function pull(){
+  var h = document.getElementById("stomach");
+h.style.animationName="sqeez";
+  h.style.animationIterationCount="3";
+  h.style.animationDuration="3s";
+  h.style.animationDelay="6s";
+}
+
+function kisses(){
+  var i = document.getElementById("lips");
+i.style.animationName="kiss";
+i.style.animationDuration="3s";
+i.style.animationIterationCount="6";
+ i.style.animationDelay="6s";    
+}
